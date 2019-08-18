c13edd0c9a4164b809a70911bc755cd2da9161b3
leandroblanco456 <leandro.blanco@alumnos.frm.utn.edu.ar>
2019-08-18 11:23:39 -0300

    agregando el chat completo

--- src/app/providers/chat.service.ts
+++ src/app/providers/chat.service.ts
@@ -65,9 +65,10 @@ export class ChatService {
 
  
   let mensaje: Mensaje = {
-    nombre: 'Demo',
+    nombre: this.usuario.nombre,
     mensaje: texto,
-    fecha: new Date().getTime()
+    fecha: new Date().getTime(),
+    uid: this.usuario.uid
   }
   return this.itemsCollection.add( mensaje );
   }
